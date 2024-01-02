502958e1f37b31399fd039c5e4be1380046261f8
JaeHwan Jeong <136150116+MiintJordy@users.noreply.github.com>
2024-01-03 08:28:06 +0900

    CallerMemberName Study

---
+++ WPF_CallerMemberName/WPF_CallerMemberName/MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF_CallerMemberName
+{
+    // CallerMemberName이 있는 경우
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set 
+            { if (_message != value)
+                {
+                    _message = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        public ICommand Btn_Click { get => new RelayCommand(Btn_Click_Func); }
+        // 버튼 클릭 이벤트 핸들러
+        private void Btn_Click_Func(object sender)
+        {
+            Message = "속성이 변경되었습니다.";
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            Console.WriteLine("CanExeCute Param : " + parameter);
+            Console.WriteLine("_canExecute : " + _canExecute);
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+
+        public void Execute(object parameter)
+        {
+            Console.WriteLine("ExeCute Param : " + parameter);
+            Console.WriteLine("_execute : " + _execute);
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ WPF_CallerMemberName/WPF_CallerMemberName/SubViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_CallerMemberName
+{
+    // CallerMemberName이 없는 경우
+    class SubViewModel : INotifyPropertyChanged
+    {
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                if(_message != value)
+                {
+                    _message = value;
+                    // [CallerMemberName]이 있을 때, OnPropertyChanged();
+                    // 즉, 속성 이름을 자동으로 첨부하는 역할임.
+                    OnPropertyChanged(Message);
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // 이 부분의 시그니처에 [CallerMemberName] 이 없다면, 위 22번 코드에서 차이점이 생김.
+        protected void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
