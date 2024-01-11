5c2ebd3ac706455eb0fae8eb0d5c003542c74d23
JaeHwan Jeong <136150116+MiintJordy@users.noreply.github.com>
2024-01-11 09:08:42 +0900

    Swallow Copy & Deep Copy

---
+++ WPF_BeforeStart3/SwallowDeepCopy/Copy.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SwallowDeepCopy
+{
+    // C++에서는 함수의 시그니처에 포인터나 참조를 전달하지 않으면 기본적으로 값이 전달됨.
+    // C#에서는 기본 자료형(int, double, float, bool, char, enum, unint 등) 같은 경우엔 값이 복사됨.
+    // 하지만, string, object, DateTime 등은 기본적으로 Reference Type으로 주소값을 전달함.
+    // 이에 따라 얕은 복사와 깊은 복사 개념이 존재함.
+    public class Copy
+    {
+        public Copy()
+        {
+            CMD_SwallowCopy = new RelayCommand(PerformCMD_SwallowCopy);
+            CMD_DeepCopy = new RelayCommand(PerformCMD_DeepCopy);
+        }
+        public class Person
+        {
+            public string Name { get; set; }
+            public Address Address { get; set; }
+        }
+
+        public class Address
+        {
+            public string City { get; set; }
+        }
+
+        public ICommand CMD_SwallowCopy { get; set; }
+        private void PerformCMD_SwallowCopy(object obj)
+        {
+            Person originalPerson = new Person { Name = "John", Address = new Address { City = "New York" } };
+            Person swallowCopyPerson = originalPerson;
+
+            swallowCopyPerson.Address.City = "Los Angeles";
+            Debug.WriteLine("얕은 복사 원본 : " + originalPerson.Address.City);
+            Debug.WriteLine("얕은 복사 복사본 : " + originalPerson.Address.City);
+        }
+
+        [Serializable]
+        public class People : ICloneable
+        {
+            public string Name { get; set; }
+            public National national { get; set; }
+
+            public object Clone()
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    IFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(stream, this);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    return formatter.Deserialize(stream);
+                }
+            }
+        }
+
+        [Serializable]
+        public class National
+        {
+            public string national { get; set; }
+        }
+
+        public ICommand CMD_DeepCopy { get; set; }
+
+        private void PerformCMD_DeepCopy(object obj)
+        {
+            People originalPerson = new People { Name = "엽상", national = new National { national = "미국" } };
+            People deepCopyPerson = (People)originalPerson.Clone();
+
+            deepCopyPerson.national.national = "한국";
+            Debug.WriteLine("깊은 복사 원본 : " + originalPerson.national.national);
+            Debug.WriteLine("깊은 복사 복사본 : " + deepCopyPerson.national.national);
+        }
+    }
+}
---
+++ WPF_BeforeStart3/SwallowDeepCopy/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SwallowDeepCopy
+{
+    internal class RelayCommand : ICommand
+    {
+        // 명령이 실행될 때, 수행할 액션을 나타내는 private 읽기 전용 필드 선언
+        private readonly Action<object> _execute;
+        // 명령이 실행 가능한지 여부를 판단하는 조건을 나타내는 private 읽기 전용 필드 선언
+        private readonly Predicate<object> _canExecute;
+
+        // RelayCommand 클래스 생성자(매개변수: Action 대리자와 실행가능 여부 판단 대리자)
+        // Action<Object> execute : 위 코드의 36번째 줄에 있는 Btn_Click_Func임.
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            // Action이 null이면 throw를 실행
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            // 실행 여부를 판단하는 대리자 할당
+            _canExecute = canExecute;
+        }
+
+        // 실행 가능 여부 반환
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        // 주어진 액션 실행
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        // UI 상태의 변경을 나타내는 이벤트가 발생함을 알림.
+        public event EventHandler CanExecuteChanged
+        {
+            // 메서드와 연결된 핸들러를 실행함.
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
